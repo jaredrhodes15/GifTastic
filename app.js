ba01c880e1a9f69d94a1cafad5d9243be222f760
Jared Rhodes <jaredrhodes15@gmail.com>
2019-01-11 04:40:57 -0700

    stopping point array wont append yet

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
+$(document).ready(function () {
+
+
+var panel = $("#moviesArr");
 var topics = ["The Departed", "Batman", "Badboys", "Primal Fear"]
 
+
+panel.append('<h6>' + topics + '</h6>');
+$("#moviesArr").prepend(topics);
+
+});
