89356319125e53764db13ac6d6d1fd69e4c27041
Jared Rhodes <jaredrhodes15@gmail.com>
2019-02-16 02:59:16 -0700

    initial buttons are now displaying. Everything is working. Could add spacing to buttons later.

--- app.js
+++ app.js
@@ -1,11 +1,57 @@
-$(document).ready(function () {
-
-
+var moviesArr = ["The Departed", "Batman", "Badboys", "Primal Fear"]
+// Not sure if below will be needed
 var panel = $("#moviesArr");
-var topics = ["The Departed", "Batman", "Badboys", "Primal Fear"]
 
-
-panel.append('<h6>' + topics + '</h6>');
-$("#moviesArr").prepend(topics);
-
+$(document).ready(function() {
+    for (var i = 0; i < moviesArr.length; i++) {
+        $("#movie-buttons").append("<button type='button' onclick='searchGif(\"" + moviesArr[i] + "\")' class='btn btn-primary' value=' " + moviesArr[i] + "'> " + moviesArr[i] + " </button>");
+    }
 });
+
+function movieButtonClicked() {
+    var userInput = $('#movie-input').val();
+    searchGif(userInput);
+}
+
+function submitButtonClicked() {
+    var userInput = $('#movie-input').val();
+
+    if (userInput) {
+        $('#movie-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
+    }
+}
+
+function searchGif(gifName) {
+    $.ajax({
+            url: 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' &api_key=dc6zaTOxFJmzC',
+            type: 'GET',
+        })
+        .done(function(response) {
+            displayGif(response);
+        })
+}
+
+function displayGif(response) {
+    $('#movies').empty();
+    for (var i = 0; i < response.data.length; i++) {
+        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
+            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
+            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
+
+        image = '<div class="col-md-4">' + image + "</div>";
+        $('#movies').append(image);
+    }
+
+    $('.movImage').on('click', function() {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).attr("data-animate"));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).attr("data-still"));
+            $(this).attr('data-state', 'still');
+        }
+
+    });
+}
